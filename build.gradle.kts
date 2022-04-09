16a2812af94cba9ed9cf86cc2eb3740ea306a464
dsyun96 <dsyun96@gmail.com>
2022-04-10 00:44:03 +0900

    thymeleaf 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    compileOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
